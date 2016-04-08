59896c08b7b694b39912a0317eb0e15858c3e627
Thibault Gigant <thibault.gigant@gmail.com>
2016-04-08 13:03:24 +0200

    Changement icone et ajout lien entre page d'accueil et page carte

--- Client/app/src/main/java/com/example/restauclient/MainActivity.java
+++ Client/app/src/main/java/com/example/restauclient/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.restauclient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +20,31 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
+
+    public void switchToFrench(View v) {
+
+    }
+
+    public void switchToEnglish(View v) {
+
+    }
+
+    public void switchToItalian(View v) {
+
+    }
+
+    public void switchToGerman(View v) {
+
+    }
+
+    public void switchToSpanish(View v) {
+
+    }
+
+    public void goToCarte(View v) {
+        Intent intent = new Intent(this, CarteActivity.class);
+        startActivity(intent);
+    }
+
+
 }
