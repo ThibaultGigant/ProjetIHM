70d4682797b800df35781ba95fe7988f9faf8c69
Thibault Gigant <thibault.gigant@gmail.com>
2016-04-09 15:12:14 +0200

    Bouton appel serveur

--- Client/app/src/main/java/com/example/restauclient/CarteActivity.java
+++ Client/app/src/main/java/com/example/restauclient/CarteActivity.java
@@ -3,8 +3,8 @@ package com.example.restauclient;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class CarteActivity extends Activity {
 
@@ -12,6 +12,16 @@ public class CarteActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carte);
+        final ImageButton mFrame = (ImageButton) findViewById(R.id.popServeur);
+
+        mFrame.post(new Runnable() {
+
+            @Override
+            public void run() {
+                mFrame.setMinimumHeight(mFrame.getWidth());
+            }
+        });
+
     }
 
     public void goToMain(View view) {
@@ -27,5 +37,11 @@ public class CarteActivity extends Activity {
     public void goToCarte(View view) {
         Intent intent = new Intent(this, CarteActivity.class);
         startActivity(intent);
+    }
+
+    public void goToBilan(View view) {
+    }
+
+    public void popServeur(View view) {
     }
 }
