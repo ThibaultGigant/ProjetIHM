c8103bccc85e9e197104fb0517714ddada4eeb89
BufferOverfl0w <cedric.hubert13@gmail.com>
2016-04-09 15:17:52 +0200

    CarteBoissons

--- Client/app/src/main/java/com/example/restauclient/CarteBoissonsActivity.java
+++ Client/app/src/main/java/com/example/restauclient/CarteBoissonsActivity.java
@@ -1,7 +1,31 @@
 package com.example.restauclient;
 
-/**
- * Created by cc on 09/04/16.
- */
-public class CarteBoissonsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+public class CarteBoissonsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carte);
+    }
+
+    public void goToMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToMenus(View view) {
+        Intent intent = new Intent(this, MenusActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToCarte(View view) {
+        Intent intent = new Intent(this, CarteActivity.class);
+        startActivity(intent);
+    }
 }
