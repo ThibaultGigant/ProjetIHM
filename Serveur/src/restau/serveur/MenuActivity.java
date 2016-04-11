7f59fef5d5204d12d5f650480fa1f17260b61904
FaycalElouariachi <faycal.eloua@gmail.com>
2016-04-11 18:03:41 +0200

    Vue du serveur : Ajout des bouttons de gestion du menu

--- Serveur/src/restau/serveur/MenuActivity.java
+++ Serveur/src/restau/serveur/MenuActivity.java
@@ -4,12 +4,14 @@ import restau.serveur.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -152,8 +154,10 @@ public class MenuActivity extends Activity {
                         nameEntree.setGravity(Gravity.LEFT);
                         entree.addView(nameEntree);
 
+                        ImageView separator = new ImageView(this);
+
                         TextView priceEntree = new TextView(this);
-                        priceEntree.setText(tableParser.getAttributeValue(tableParser.getNamespace(), "price"));
+                        priceEntree.setText("   " + tableParser.getAttributeValue(tableParser.getNamespace(), "price"));
                         //priceEntree.setText(getResId(tableParser.getAttributeValue(tableParser.getNamespace(), "price"), R.id.class));
                         priceEntree.setBackground(null);
                         priceEntree.setGravity(Gravity.RIGHT);
