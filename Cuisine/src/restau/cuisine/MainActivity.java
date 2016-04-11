6e42a8d5ffbf30dfc6bb6649bf2228525b4046c4
Keithdae <keithdae@gmail.com>
2016-04-11 19:40:52 +0200

    Ajout de menu pour valider les plats, ajout des warning

--- Cuisine/src/restau/cuisine/MainActivity.java
+++ Cuisine/src/restau/cuisine/MainActivity.java
@@ -1,16 +1,28 @@
 package restau.cuisine;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.Activity;
 import android.os.SystemClock;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
 import android.widget.Chronometer;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends Activity{
 
     private ImageView[] chronoIcons = new ImageView[8];
-    private Chronometer[] chronometers = new Chronometer[8];
+    private int[] chronometersID = {R.id.chronometer1, R.id.chronometer2, R.id.chronometer3, R.id.chronometer4, R.id.chronometer5, R.id.chronometer6, R.id.chronometer7, R.id.chronometer8};
+    private int[] textDishID = {R.id.entrees1_1, R.id.entrees1_2, R.id.plats1_1, R.id.plats1_2, R.id.desserts1_1, R.id.desserts1_2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,27 +46,36 @@ public class MainActivity extends Activity {
         }
 
         // Initialise les chronometres
-        startAllChronos();
+        startAllChronos(chronometersID);
+
+        // TextView clickable
+        multiTextClickable(textDishID);
+
+        // Ajout des warnings
+        setWarning(R.id.warningEntrees1_1, "Avec supplément de sucre et de piment");
+
+        // Mise en place du reset pour la table
+        setupClear(R.id.textView1, R.id.scrollView1);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu (Menu menu){
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
 
-    private void startChrono(int id)
-    {
-        Chronometer timeElapsed  = (Chronometer) findViewById(id);
-        timeElapsed.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener(){
+    private void startChrono(int id) {
+        Chronometer timeElapsed = (Chronometer) findViewById(id);
+        timeElapsed.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer cArg) {
                 long time = SystemClock.elapsedRealtime() - cArg.getBase();
                 int h   = (int)(time /3600000);
                 int m = (int)(time - h*3600000)/60000;
                 String mm = m < 10 ? "0"+m: m+"";
-                cArg.setText(mm+" minutes");
+                mm += " minutes";
+                cArg.setText(mm);
             }
         });
         timeElapsed.setBase(SystemClock.elapsedRealtime());
@@ -62,15 +83,100 @@ public class MainActivity extends Activity {
     }
 
 
-    private void startAllChronos()
+    private void startAllChronos(int[] id)
     {
-        startChrono(R.id.chronometer1);
-        startChrono(R.id.chronometer2);
-        startChrono(R.id.chronometer3);
-        startChrono(R.id.chronometer4);
-        startChrono(R.id.chronometer5);
-        startChrono(R.id.chronometer6);
-        startChrono(R.id.chronometer7);
-        startChrono(R.id.chronometer8);
+        for(int i : id)
+        {
+            startChrono(i);
+        }
     }
+
+    private void textClickable(int id)
+    {
+        RelativeLayout lay = (RelativeLayout) findViewById(id);
+        int count = lay.getChildCount();
+        View v;
+        TextView text = null;
+        for(int i=0; i<count; i++) {
+            v = lay.getChildAt(i);
+            if(v instanceof TextView)
+            {
+                text = (TextView) v;
+            }
+        }
+
+        if(text != null) {
+            text.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    PopupMenu popup = new PopupMenu(getBaseContext(), v);
+                    popup.getMenuInflater().inflate(R.menu.popup, popup.getMenu());
+                    popup.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            lay.removeAllViews();
+                            return true;
+                        }
+                    });
+
+                    popup.show();
+                }
+            });
+        }
+    }
+
+    private void multiTextClickable(int[] id)
+    {
+        for(int i : id)
+        {
+            textClickable(i);
+        }
+    }
+
+
+    private void setWarning(int idWarningImage, String warning)
+    {
+        ImageView img = (ImageView) findViewById(idWarningImage);
+        img.setImageResource(R.drawable.warning);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(warning)
+                        .setTitle("Attention");
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+    }
+
+
+    private void setupClear(int idTextView, int idScrollArea)
+    {
+        TextView text = (TextView) findViewById(idTextView);
+        ScrollView scroll = (ScrollView) findViewById(idScrollArea);
+
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(getBaseContext(), v);
+                popup.getMenuInflater().inflate(R.menu.popupclear, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        scroll.removeAllViews();
+                        return true;
+                    }
+                });
+
+                popup.show();
+            }
+        });
+
+    }
+
 }
