141d9cacb700475718e72dd6f7fa6e89250a55c9
Thibault Gigant <thibault.gigant@gmail.com>
2016-04-12 02:08:47 +0200

    Correction imports Serveur

--- Serveur/src/restau/serveur/MenuActivity.java
+++ Serveur/src/restau/serveur/MenuActivity.java
@@ -1,15 +1,8 @@
 package restau.serveur;
 
-import restau.serveur.util.SystemUiHider;
-
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.media.Image;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
